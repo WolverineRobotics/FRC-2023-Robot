d0ddb745b8d706615e546d2b196f797403a0222d
Khalil Balde <33702527+Khalil-Balde1212@users.noreply.github.com>
2023-03-16 22:05:18 -0400

    Revert "Ouen "

--- src/main/java/frc/robot/commands/DefaultIntakeCommand.java
+++ src/main/java/frc/robot/commands/DefaultIntakeCommand.java
@@ -1,55 +1,12 @@
-/*
- * PLEASE CHECK OVER I DONT KNOW WHAT IM DOING
- */
-
-
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.subsystems.IntakeSubsystem;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.OI;
 
 public class DefaultIntakeCommand extends CommandBase{
     private IntakeSubsystem s_intake;
-
     //TELEOP CONTROLS GO HERE    
     public DefaultIntakeCommand(IntakeSubsystem intake){
         s_intake = intake;
-        addRequirements(s_intake);
-
-    }
-
-    @Override
-    public void initialize() {
-
-    }
-
-    @Override
-    public void execute() {
-        // Opening and Closing Claw -- Cone and Release Cone
-        if (OI.takeCone()) {
-            s_intake.openClaw();
-        }
-
-        if (OI.releaseCone()) {
-            s_intake.closeClaw();
-        }
-
-        // Grab and Release Cube
-        if (OI.takeCube() && !s_intake.hasCube()) {
-            s_intake.OperateIntake(-1);
-        } 
-
-        if (OI.releaseCube() && s_intake.hasCube()) {
-            s_intake.OperateIntake(1);
-        }
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
     }
 }
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,40 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class IntakeSubsystem extends SubsystemBase {
     //DO NOT PUT CONTROLLER STUFF IN HERE
-    private final WPI_TalonSRX intakeMotor;    
-    private final DoubleSolenoid pistons;
-    private final DigitalInput limitSwitch;
-
-    public IntakeSubsystem() {
-        intakeMotor = new WPI_TalonSRX(Constants.INTAKE_MOTOR);
-        pistons = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0);
-        limitSwitch = new DigitalInput(0);
-
-    }
-
-    public boolean hasCube() {
-        return limitSwitch.get();
-    }
-
-    public void openClaw() {
-        pistons.set(Value.kForward);
-    }
-
-    public void closeClaw() {
-        pistons.set(Value.kReverse);
-    }
-
-    public void OperateIntake(double speed){
-        intakeMotor.set(speed);
-
-    }
 }
