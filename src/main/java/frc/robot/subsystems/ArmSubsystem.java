dea50d6f26ebbb784260373f4b6ff6cd8eb02799
LocalStore <84473197+YourLocalStore@users.noreply.github.com>
2023-03-29 14:29:47 -0400

    Additions for Elevator Components

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,5 +4,5 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase{
     //DO NOT PUT CONTROLLER STUFF IN HERE
-    //WE ARE NOT USING THE ARM ANYMORE - JOSHUA
+    //WE ARE NOT USING AN ARM SYSTEM - JOSHUA
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import com.ctre.phoenix.motorcontrol.can.WPI_CANSparkMax;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import frc.robot.commands.elevator.defaultElevatorCommand;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -18,30 +18,52 @@ public class ElevatorSubsystem extends SubsystemBase{
     private int elevatorPosition = 0
 
     public ElevatorSystem(){
-
-        l_motor = new CANSparkMax();
-        r_motor = new CANSparkMax();
+        
+        // Motors
+        l_motor = new CANSparkMax(Constants.ELEVATOR_MOTOR_A);
+        r_motor = new CANSparkMax(Constants.ELEVATOR_MOTOR_B);
         m_motor = new MotorControllerGroup(l_motor, r_motor);
 
-        upperelevatorlimit = new DigitalInput(0);
-        lowerelevatorlimit = new DigitalInput(1);
+        // Limit Switches
+        lowerelevatorlimit = new DigitalInput(0);
+        upperelevatorlimit = new DigitalInput(1);
 
-        // Motor Functions
+        // Architecture says there's an encoder so I'll just leave this here
+        elevatorEncoder = new Encoder(Constants.ELEVATOR_ENCODER_A, false, EncodingType.k2X);
+
+        // Motor Speed
         public void setSpeed(double speed){
             m_motor.set(speed);
         }
 
-        public int getEncoderPosition() {
-            return elevatorEncoder.get();
-        }   
+        // Checks for Elevator Positioning from Limit Switches
+        public boolean getUpperLimitSwitch(){
+            return upperelevatorlimit.get();
+        }  
 
+        public boolean getLowerLimitSwitch(){
+            return lowerelevatorlimit.get(); 
+        }  
+        
         public void PositionCheck(){
             if (lowerelevatorlimit.get()) {
-                elevatorPosition = 0;
+                elevatorPosition = 0; // Bottom of Elevator
             }
             else if (upperelevatorlimit.get()){
-                elevatorPosition = 2; 
+                elevatorPosition = 2; // Top of Elevator
             }
+            else {
+                elevatorPosition = 1; // "Approximately" the Middle
+            }
+        }
+
+        // Elevator Encoder Stuff -- I can remove this if not needed
+        public int getEncoderPosition(){
+            return elevatorEncoder.get();
+        } 
+
+        public double getEncoderDistance(){
+            return elevatorEncoder.getDistance();
         }
     }
 }
