d965936be6a8719f96084c9f8a3d6e33808e99c2
LocalStore <84473197+YourLocalStore@users.noreply.github.com>
2023-03-29 10:40:55 -0400

    Elevator Stuff

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,4 +4,5 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase{
     //DO NOT PUT CONTROLLER STUFF IN HERE
+    //WE ARE NOT USING THE ARM ANYMORE - JOSHUA
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,7 +1,45 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import com.ctre.phoenix.motorcontrol.can.WPI_CANSparkMax;
+import frc.robot.commands.elevator.defaultElevatorCommand;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase{
-    //DO NOT PUT CONTROLLER STUFF IN HERE
+    
+    private final Encoder elevatorEncoder;
+    private final CANSparkMax l_motor, r_motor; 
+    private final MotorControllerGroup m_motor;
+    private final DigitalInput upperelevatorlimit, lowerelevatorlimit; 
+
+    private int elevatorPosition = 0
+
+    public elevatorSystem(){
+
+        l_motor = new CANSparkMax();
+        r_motor = new CANSparkMax();
+        m_motor = new MotorControllerGroup(l_motor, r_motor);
+
+        toplimitswitch = new DigitalInput(0);
+        bottomlimitswitch = new DigitalInput(1);
+
+        // Motor Functions
+        public void setSpeed(double speed){
+            m_motor.set(speed);
+        }
+
+        public int getEncoderPosition() {
+            return elevatorEncoder.get();
+        }   
+
+        public void positionCheck(){
+            if lowerelevatorlimit.get()
+                elevatorPosition = 0;
+            else if upperelevatorlimit.get()
+                elevatorPosition = 2; 
+        }
+    }
 }
